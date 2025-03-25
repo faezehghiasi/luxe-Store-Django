b12e674f50049cf7158fbb888232e5e3b76c7582
faezehghiasi <f.ghiasi@ec.iut.ac.ir>
2025-03-25 10:29:09 +0330

    Improved class structures

--- store/core/models.py
+++ store/core/models.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.utils.text import slugify
 import uuid
 
-class Base:
+class Base(models.Model):
     create_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
     uuid = models.UUIDField(unique=True, default=uuid.uuid4) #for security #for default we sent function not value
     deleted = models.BooleanField(default=False)
     deleted_date = models.DateField(default=None, null=True, blank=True)
+    class Meta:
+        abstract = True
 #****************************************************************************************************************************
-class Product(models.Model,Base):
+class Product(Base):
     
     # STATUS_ENABLED = 0
     # STATUS_DISABLED = 1
     # STATUS_DELETED = 2
     # STATUS_CHOICES = ((STATUS_ENABLED,'Enabled'),(STATUS_DISABLED , 'Disabled') , (STATUS_DELETED , 'Deleted'))
+
     name = models.CharField(max_length=300)
     price = models.IntegerField(default=0)
     discount = models.FloatField(default=0)
@@ -28,30 +31,31 @@ class Product(models.Model,Base):
 
 
     description = models.TextField()
-    # category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products')
+
+
     # old_category = models.ForeignKey('Category',on_delete=models.PROTECT,related_name='old_products')
-    
     #این اسم هایی که میدیم برای رابطه های معکوس است که جنگو خودش درست میکند اگه اسم بش ندیم دوتا اسم یکسان درست میکنه و ارور میده موقع مایگریت کردن
     #if we enter argument in '' we can declare class later
 
 #****************************************************************************************************************************
-class Category(models.Model,Base):
+class Category(Base):
     name = models.CharField(max_length=300)
     #product_set = QuerySet //برای رابطه ی معکوس 
 
 
 #****************************************************************************************************************************
-class Tag(models.Model,Base):
+class Tag(Base):
     name = models.CharField(max_length=300)
 
 
 #****************************************************************************************************************************
-class Comment(models.Model,Base):
+class Comment(Base):
     name = models.CharField(max_length=300)
 
 
 #****************************************************************************************************************************
-class Like(models.Model,Base):
+class Like(Base):
     pass
 
     
