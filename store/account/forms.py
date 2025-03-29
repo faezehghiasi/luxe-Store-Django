a4bf17e2167dfb35b7455ce5481a608597ff6449
faezehghiasi <f.ghiasi@ec.iut.ac.ir>
2025-03-30 02:25:33 +0330

    change signup form

--- store/account/forms.py
+++ store/account/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.core import validators
 from phonenumber_field.formfields import PhoneNumberField
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+
 
 #*********************************************************************************************************
 # class SignUpForm(forms.Form):
@@ -15,12 +17,13 @@ from . import models
 #
 #*********************************************************************************************************
 
-class SignUpForm(forms.ModelForm):
+class SignUpForm(UserCreationForm):
     class Meta:
         model = models.User
-        fields = ['first_name', 'last_name', 'email','phone_number','password']
+        exclude = ['is_staff', 'is_superuser', 'is_active', 'date_joined', 'groups', 'user_permissions','last_login','avatar']
+        # fields = ['first_name', 'last_name', 'email','phone_number','password']
         # fields = '__all__'
-        # exclude = ['is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions']
+
 
 # *********************************************************************************************************
 
