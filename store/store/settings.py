425c66232f09df3f506c8a92cd71dc63dc8674f5
faezehghiasi <f.ghiasi@ec.iut.ac.ir>
2025-07-15 15:41:00 +0330

    fix bug

--- store/store/settings.py
+++ store/store/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storedb',
+        'USER': 'storeuser',
+        'PASSWORD': 'storepass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
